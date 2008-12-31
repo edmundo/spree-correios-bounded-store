03973109d186f4abc063de376cc2167b3b88e21f
Edmundo Valle Neto <edmundo.vn@gmail.com>
2008-12-31 17:47:14 -0200

    Added support for internationalization.

--- correios_bounded_store_extension.rb
+++ correios_bounded_store_extension.rb
@@ -1,10 +1,10 @@
 # Uncomment this if you reference any of your controllers in activate
-# require_dependency 'application'
+require_dependency 'application'
 
 class CorreiosBoundedStoreExtension < Spree::Extension
-  version "1.0"
-  description "Describe your extension here"
-  url "http://yourwebsite.com/correios_bounded_store"
+  version "0.99"
+  description "Support for brazilian postal service online shiping price calculation, using a type of web service access contract called 'bounded store'."
+  url "http://github.com/edmundo/spree-correios-bounded-store/tree/master"
 
   # Please use correios_bounded_store/config/routes.rb instead for extension routes.
 
@@ -13,6 +13,15 @@ class CorreiosBoundedStoreExtension < Spree::Extension
   # end
   
   def activate
-    # admin.tabs.add "Correios Bounded Store", "/admin/correios_bounded_store", :after => "Layouts", :visibility => [:all]
+    # Add support for internationalization to this extension.
+    Globalite.add_localization_source(File.join(RAILS_ROOT, 'vendor/extensions/correios_bounded_store/lang/ui'))
+
+    # Add the administration link. (Only as a placeholder)
+    Admin::ConfigurationsController.class_eval do
+      before_filter :add_correios_bounded_store_link, :only => :index
+      def add_correios_bounded_store_link
+        @extension_links << {:link => "#" , :link_text => Globalite.localize(:ext_correios_bounded_store), :description => Globalite.localize(:ext_correios_bounded_store_description)}
+      end
+    end
   end
 end
